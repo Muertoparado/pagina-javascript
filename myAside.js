bfda6ef2f78a7c6f22245f05fbab60d60444309e
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-23 09:29:03 -0500

    feat(PAGINA JAVASCRIPT): :lipstick: scroll

--- myAside.js
+++ myAside.js
@@ -5,8 +5,8 @@ export default{
             paragranph: "",
         },
        
-    }
-}
+    },
+
 nav:[
     {
         title:"archives",
@@ -24,6 +24,36 @@ nav:[
             }
         ]
     },
-]
+],
+
+showAside() {
+    const data = this.nav.map((val,id)=>{
+        return(
+            (val.link)
+            ? this.list(val)
+            : this.card(val)
+        );
+    });
+    document.querySelector("#nav").insertAdjacentHTML("beforeend",
+    data.join(""))},
+
+    cards(p1){
+        return`  <div class="p-4 mb-3 bg-light rounded">
+        <h4 class="fst-italic">${p1.title}</h4>
+        <p class="mb-0">${p1.paragranph}</p>
+      </div>`
+      ;
+    },
+    list (p1){
+        return ` <div class="p-4">
+        <h4 class="fst-italic">${p1.title}</h4>
+        <ol class="list-unstyled">${p1.linl.map((val,id)=> `<li><a href="${val.href}">GitHub</a></li>)}
+          ${val.name}</a></li>`).join("")}
+          </ol>
+        </div>`
+        ;
+    }
+}
+
 
    
